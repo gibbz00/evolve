f43cb72b23dd8be58fe4387026e8bbfa42853057
Gabriel Hansson <gabrielhansson00@gmail.com>
2022-08-22 21:58:10 +0000

    mirror server sorting and packages file functionality

--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 source ./env.sh
-# TODO: give it internet access
-docker run --rm $IMAGE_NAME
+
+# give it internet access, done with --net=expose?
+
+# --privilged flag to allow root access to devices?
+
+# using bind mounts for faster persistent storage?
+#   https://docs.docker.com/storage/bind-mounts/
+docker run --rm -it $IMAGE_NAME /bin/sh
---
+++ src/setup.sh
@@ -0,0 +1,23 @@
+# Following the recommendations from:
+# https://hub.docker.com/_/archlinux/
+ set -xe; \
+    pacman-key --init; \
+    pacman -Syyu --noconfirm;
+
+# Select mirror servers by download rate
+# pacman-contrib includes rankmirrors script
+pacman -S pacman-contrib --noconfirm --needed;
+cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+curl -s "https://archlinux.org/mirrorlist/?country=SE&protocol=https&use_mirror_status=on" \
+| sed -e 's/^#Server/Server/' -e '/^#/d' \
+| rankmirrors -v -n 5 - \
+> /etc/pacman.d/mirrorlist
+
+# Install packages
+PACKAGES=$(sed -e '/^#/d' "$BUILD_DIRECTORY/packages" | tr '\n' ' ')
+pacman -S $PACKAGES --noconfirm --needed
+
+# Add root password and user credentials
+# useradd -m -G users,wheel username
+# echo "username:password" | chpasswd
+# passwd
