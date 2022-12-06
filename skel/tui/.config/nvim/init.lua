92c4bd53d0964d5d19fca411f767f80ab34fe618
gibbz00 <gabrielhansson00@gmail.com>
2022-12-06 18:25:48 +0100

    neovim language serves extension handling

--- skel/tui/.config/nvim/init.lua
+++ skel/tui/.config/nvim/init.lua
@@ -15,7 +15,26 @@ vim.opt.splitbelow = true
 
 vim.g.mapleader = ' '
 
+-- vim.keymay.set('mode', 'key', 'value')
+
+-- Save
+vim.keymap.set('n', '<leader>s', '<CMD>update<CR>')
+
+-- Borne keyboard specific
+vim.keymap.set('n', '<S-/>', '?')
+-- Jump backwards to end of word
+vim.keymap.set('n', 'h', 'ge')
+vim.keymap.set('n', 'k', 'gE')
+
 require('ayu').colorscheme()
+require("mason").setup()
+require("mason-lspconfig").setup()
+-- :h mason-lspconfig-autoamtic-server-setup
+require("mason-lspconfig").setup_handlers {
+        function (server_name)
+            require("lspconfig")[server_name].setup {}
+        end
+}
 
 -- until these are fixed
 -- https://github.com/neovim/neovim/issues/8350
