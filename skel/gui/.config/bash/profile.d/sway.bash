8e87f4858095efda9eea73fa2a2b7174aed8eb02
gibbz00 <gabrielhansson00@gmail.com>
2023-03-12 01:46:50 +0100

    Retain rofi usage in screenshot tool
    sway-interactive-screenshot moved to fuzzel. Forked project
    and reverted this.

--- skel/gui/.config/bash/profile.d/sway.bash
+++ skel/gui/.config/bash/profile.d/sway.bash
@@ -1,6 +1,10 @@
 #!/bin/bash
 export WALLPAPER="$HOME/media/wallpapers/fish.jpg"
 
+_screenshot_dir="/tmp/screenshots"
+mkdir --parents "$_screenshot_dir"
+export SWAY_ROFI_SCREENSHOT_SAVEDIR="$_screenshot_dir"
 # used in sway config
 export XDG_CONFIG_HOME="$HOME/.config"
+
 ## Sway TTY startup inserted from evolve/setup/gui.sh 
