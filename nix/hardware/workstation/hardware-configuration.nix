86f5363bdde951bd41ddf1b476953e7790f95cdd
gibbz00 <gabrielhansson00@gmail.com>
2024-08-10 15:07:39 +0200

    feat(nix): add `workstation` hardware configuration

---
+++ nix/hardware/workstation/hardware-configuration.nix
@@ -0,0 +1,15 @@
+{ config, lib, modulesPath, ... }:
+
+{
+  imports =[ (modulesPath + "/installer/scan/not-detected.nix") ];
+
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  networking.useDHCP = lib.mkDefault true;
+}
